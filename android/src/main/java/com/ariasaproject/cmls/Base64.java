a162f52c1b4fdc0e173277c8d17a339ea31f8731
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-06-17 13:41:08 +0800

    commit 11

--- android/src/main/java/com/ariasaproject/cmls/Base64.java
+++ android/src/main/java/com/ariasaproject/cmls/Base64.java
@@ -1,11 +1,6 @@
 package com.ariasaproject.cmls;
 
-/**
- * Created by Ben David on 01/08/2017.
- */
-
-public class Base64
-{
+public class Base64 {
     private final static char[] BASE64_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".toCharArray();
     public final static String encode(String str)
     {
