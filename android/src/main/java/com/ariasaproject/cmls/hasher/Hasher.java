d80cae3930983e971c3df036cc1cbdaad614619b
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-06-23 08:00:32 +0800

    Hasher 2

--- android/src/main/java/com/ariasaproject/cmls/hasher/Hasher.java
+++ android/src/main/java/com/ariasaproject/cmls/hasher/Hasher.java
@@ -14,7 +14,8 @@ public class Hasher {
     private byte[] B = new byte[132]; //128 + 4
     private int[] X = new int[32];
     private int[] V = new int[32768]; //32 * 1024
-    int i, j, k;
+    int[] xs = new int[16];
+    int i, j, k, l;
 
     public Hasher() throws GeneralSecurityException {
         mac = Mac.getInstance("HmacSHA256");
@@ -42,14 +43,16 @@ public class Hasher {
             }
         }
 
-        for (i = 0; i < 1024; i++) {
-            arraycopy(X, 0, V, i * 32, 32);
+        for (i = 0; i < 32768; i+=32) { // 1024*32
+            arraycopy(X, 0, V, i, 32);
+            //xorSalsa8
             xorSalsa82();
         }
         for (i = 0; i < 1024; i++) {
             k = (X[16] & 1023) * 32;
             for (j = 0; j < 32; j++)
                 X[j] ^= V[k + j];
+            //xorSalsa8
             xorSalsa82();
         }
 
@@ -65,114 +68,107 @@ public class Hasher {
         mac.doFinal(H, 0);
         return H;
     }
-    int[] xs = new int[16];
-    private void xorSalsa82() {
-        int di = 0, xi = 16;
-        {
-            xs[0] = (X[di +  0] ^= X[xi +  0]);
-            xs[1] = (X[di +  1] ^= X[xi +  1]);
-            xs[2] = (X[di +  2] ^= X[xi +  2]);
-            xs[3] = (X[di +  3] ^= X[xi +  3]);
-            xs[4] = (X[di +  4] ^= X[xi +  4]);
-            xs[5] = (X[di +  5] ^= X[xi +  5]);
-            xs[6] = (X[di +  6] ^= X[xi +  6]);
-            xs[7] = (X[di +  7] ^= X[xi +  7]);
-            xs[8] = (X[di +  8] ^= X[xi +  8]);
-            xs[9] = (X[di +  9] ^= X[xi +  9]);
-            xs[10] = (X[di + 10] ^= X[xi + 10]);
-            xs[11] = (X[di + 11] ^= X[xi + 11]);
-            xs[12] = (X[di + 12] ^= X[xi + 12]);
-            xs[13] = (X[di + 13] ^= X[xi + 13]);
-            xs[14] = (X[di + 14] ^= X[xi + 14]);
-            xs[15] = (X[di + 15] ^= X[xi + 15]);
-            for (int i = 0; i < 8; i += 2) {
-                xs[4] ^= rotateLeft(xs[0]+xs[12], 7);  xs[8] ^= rotateLeft(xs[4]+xs[0], 9);
-                xs[12] ^= rotateLeft(xs[8]+xs[4],13);  xs[0] ^= rotateLeft(xs[12]+xs[8],18);
-                xs[9] ^= rotateLeft(xs[5]+xs[1], 7);  xs[13] ^= rotateLeft(xs[9]+xs[5], 9);
-                xs[1] ^= rotateLeft(xs[13]+xs[9],13);  xs[5] ^= rotateLeft(xs[1]+xs[13],18);
-                xs[14] ^= rotateLeft(xs[10]+xs[6], 7);  xs[2] ^= rotateLeft(xs[14]+xs[10], 9);
-                xs[6] ^= rotateLeft(xs[2]+xs[14],13);  xs[10] ^= rotateLeft(xs[6]+xs[2],18);
-                xs[3] ^= rotateLeft(xs[15]+xs[11], 7);  xs[7] ^= rotateLeft(xs[3]+xs[15], 9);
-                xs[11] ^= rotateLeft(xs[7]+xs[3],13);  xs[15] ^= rotateLeft(xs[11]+xs[7],18);
-                xs[1] ^= rotateLeft(xs[0]+xs[3], 7);  xs[2] ^= rotateLeft(xs[1]+xs[0], 9);
-                xs[3] ^= rotateLeft(xs[2]+xs[1],13);  xs[0] ^= rotateLeft(xs[3]+xs[2],18);
-                xs[6] ^= rotateLeft(xs[5]+xs[4], 7);  xs[7] ^= rotateLeft(xs[6]+xs[5], 9);
-                xs[4] ^= rotateLeft(xs[7]+xs[6],13);  xs[5] ^= rotateLeft(xs[4]+xs[7],18);
-                xs[11] ^= rotateLeft(xs[10]+xs[9], 7);  xs[8] ^= rotateLeft(xs[11]+xs[10], 9);
-                xs[9] ^= rotateLeft(xs[8]+xs[11],13);  xs[10] ^= rotateLeft(xs[9]+xs[8],18);
-                xs[12] ^= rotateLeft(xs[15]+xs[14], 7);  xs[13] ^= rotateLeft(xs[12]+xs[15], 9);
-                xs[14] ^= rotateLeft(xs[13]+xs[12],13);  xs[15] ^= rotateLeft(xs[14]+xs[13],18);
-            }
-            X[di +  0] += xs[0];
-            X[di +  1] += xs[1];
-            X[di +  2] += xs[2];
-            X[di +  3] += xs[3];
-            X[di +  4] += xs[4];
-            X[di +  5] += xs[5];
-            X[di +  6] += xs[6];
-            X[di +  7] += xs[7];
-            X[di +  8] += xs[8];
-            X[di +  9] += xs[9];
-            X[di + 10] += xs[10];
-            X[di + 11] += xs[11];
-            X[di + 12] += xs[12];
-            X[di + 13] += xs[13];
-            X[di + 14] += xs[14];
-            X[di + 15] += xs[15];
+    private final void xorSalsa82() {
+        xs[0] = (X[0] ^= X[16]);
+        xs[1] = (X[1] ^= X[17]);
+        xs[2] = (X[2] ^= X[18]);
+        xs[3] = (X[3] ^= X[19]);
+        xs[4] = (X[4] ^= X[20]);
+        xs[5] = (X[5] ^= X[21]);
+        xs[6] = (X[6] ^= X[22]);
+        xs[7] = (X[7] ^= X[23]);
+        xs[8] = (X[8] ^= X[24]);
+        xs[9] = (X[9] ^= X[25]);
+        xs[10] = (X[10] ^= X[26]);
+        xs[11] = (X[11] ^= X[27]);
+        xs[12] = (X[12] ^= X[28]);
+        xs[13] = (X[13] ^= X[29]);
+        xs[14] = (X[14] ^= X[30]);
+        xs[15] = (X[15] ^= X[31]);
+        for (l = 0; l < 4; l++) { // 8/2
+            xs[4] ^= rotateLeft(xs[0]+xs[12], 7);  xs[8] ^= rotateLeft(xs[4]+xs[0], 9);
+            xs[12] ^= rotateLeft(xs[8]+xs[4],13);  xs[0] ^= rotateLeft(xs[12]+xs[8],18);
+            xs[9] ^= rotateLeft(xs[5]+xs[1], 7);  xs[13] ^= rotateLeft(xs[9]+xs[5], 9);
+            xs[1] ^= rotateLeft(xs[13]+xs[9],13);  xs[5] ^= rotateLeft(xs[1]+xs[13],18);
+            xs[14] ^= rotateLeft(xs[10]+xs[6], 7);  xs[2] ^= rotateLeft(xs[14]+xs[10], 9);
+            xs[6] ^= rotateLeft(xs[2]+xs[14],13);  xs[10] ^= rotateLeft(xs[6]+xs[2],18);
+            xs[3] ^= rotateLeft(xs[15]+xs[11], 7);  xs[7] ^= rotateLeft(xs[3]+xs[15], 9);
+            xs[11] ^= rotateLeft(xs[7]+xs[3],13);  xs[15] ^= rotateLeft(xs[11]+xs[7],18);
+            xs[1] ^= rotateLeft(xs[0]+xs[3], 7);  xs[2] ^= rotateLeft(xs[1]+xs[0], 9);
+            xs[3] ^= rotateLeft(xs[2]+xs[1],13);  xs[0] ^= rotateLeft(xs[3]+xs[2],18);
+            xs[6] ^= rotateLeft(xs[5]+xs[4], 7);  xs[7] ^= rotateLeft(xs[6]+xs[5], 9);
+            xs[4] ^= rotateLeft(xs[7]+xs[6],13);  xs[5] ^= rotateLeft(xs[4]+xs[7],18);
+            xs[11] ^= rotateLeft(xs[10]+xs[9], 7);  xs[8] ^= rotateLeft(xs[11]+xs[10], 9);
+            xs[9] ^= rotateLeft(xs[8]+xs[11],13);  xs[10] ^= rotateLeft(xs[9]+xs[8],18);
+            xs[12] ^= rotateLeft(xs[15]+xs[14], 7);  xs[13] ^= rotateLeft(xs[12]+xs[15], 9);
+            xs[14] ^= rotateLeft(xs[13]+xs[12],13);  xs[15] ^= rotateLeft(xs[14]+xs[13],18);
         }
-        di = 16;
-        xi = 0;
-        {
-            xs[0] = (X[di +  0] ^= X[xi +  0]);
-            xs[1] = (X[di +  1] ^= X[xi +  1]);
-            xs[2] = (X[di +  2] ^= X[xi +  2]);
-            xs[3] = (X[di +  3] ^= X[xi +  3]);
-            xs[4] = (X[di +  4] ^= X[xi +  4]);
-            xs[5] = (X[di +  5] ^= X[xi +  5]);
-            xs[6] = (X[di +  6] ^= X[xi +  6]);
-            xs[7] = (X[di +  7] ^= X[xi +  7]);
-            xs[8] = (X[di +  8] ^= X[xi +  8]);
-            xs[9] = (X[di +  9] ^= X[xi +  9]);
-            xs[10] = (X[di + 10] ^= X[xi + 10]);
-            xs[11] = (X[di + 11] ^= X[xi + 11]);
-            xs[12] = (X[di + 12] ^= X[xi + 12]);
-            xs[13] = (X[di + 13] ^= X[xi + 13]);
-            xs[14] = (X[di + 14] ^= X[xi + 14]);
-            xs[15] = (X[di + 15] ^= X[xi + 15]);
-            for (int i = 0; i < 8; i += 2) {
-                xs[4] ^= rotateLeft(xs[0]+xs[12], 7);  xs[8] ^= rotateLeft(xs[4]+xs[0], 9);
-                xs[12] ^= rotateLeft(xs[8]+xs[4],13);  xs[0] ^= rotateLeft(xs[12]+xs[8],18);
-                xs[9] ^= rotateLeft(xs[5]+xs[1], 7);  xs[13] ^= rotateLeft(xs[9]+xs[5], 9);
-                xs[1] ^= rotateLeft(xs[13]+xs[9],13);  xs[5] ^= rotateLeft(xs[1]+xs[13],18);
-                xs[14] ^= rotateLeft(xs[10]+xs[6], 7);  xs[2] ^= rotateLeft(xs[14]+xs[10], 9);
-                xs[6] ^= rotateLeft(xs[2]+xs[14],13);  xs[10] ^= rotateLeft(xs[6]+xs[2],18);
-                xs[3] ^= rotateLeft(xs[15]+xs[11], 7);  xs[7] ^= rotateLeft(xs[3]+xs[15], 9);
-                xs[11] ^= rotateLeft(xs[7]+xs[3],13);  xs[15] ^= rotateLeft(xs[11]+xs[7],18);
-                xs[1] ^= rotateLeft(xs[0]+xs[3], 7);  xs[2] ^= rotateLeft(xs[1]+xs[0], 9);
-                xs[3] ^= rotateLeft(xs[2]+xs[1],13);  xs[0] ^= rotateLeft(xs[3]+xs[2],18);
-                xs[6] ^= rotateLeft(xs[5]+xs[4], 7);  xs[7] ^= rotateLeft(xs[6]+xs[5], 9);
-                xs[4] ^= rotateLeft(xs[7]+xs[6],13);  xs[5] ^= rotateLeft(xs[4]+xs[7],18);
-                xs[11] ^= rotateLeft(xs[10]+xs[9], 7);  xs[8] ^= rotateLeft(xs[11]+xs[10], 9);
-                xs[9] ^= rotateLeft(xs[8]+xs[11],13);  xs[10] ^= rotateLeft(xs[9]+xs[8],18);
-                xs[12] ^= rotateLeft(xs[15]+xs[14], 7);  xs[13] ^= rotateLeft(xs[12]+xs[15], 9);
-                xs[14] ^= rotateLeft(xs[13]+xs[12],13);  xs[15] ^= rotateLeft(xs[14]+xs[13],18);
-            }
-            X[di +  0] += xs[0];
-            X[di +  1] += xs[1];
-            X[di +  2] += xs[2];
-            X[di +  3] += xs[3];
-            X[di +  4] += xs[4];
-            X[di +  5] += xs[5];
-            X[di +  6] += xs[6];
-            X[di +  7] += xs[7];
-            X[di +  8] += xs[8];
-            X[di +  9] += xs[9];
-            X[di + 10] += xs[10];
-            X[di + 11] += xs[11];
-            X[di + 12] += xs[12];
-            X[di + 13] += xs[13];
-            X[di + 14] += xs[14];
-            X[di + 15] += xs[15];
+        X[0] += xs[0];
+        X[1] += xs[1];
+        X[2] += xs[2];
+        X[3] += xs[3];
+        X[4] += xs[4];
+        X[5] += xs[5];
+        X[6] += xs[6];
+        X[7] += xs[7];
+        X[8] += xs[8];
+        X[9] += xs[9];
+        X[10] += xs[10];
+        X[11] += xs[11];
+        X[12] += xs[12];
+        X[13] += xs[13];
+        X[14] += xs[14];
+        X[15] += xs[15];
+        
+        xs[0] = (X[16] ^= X[0]);
+        xs[1] = (X[17] ^= X[1]);
+        xs[2] = (X[18] ^= X[2]);
+        xs[3] = (X[19] ^= X[3]);
+        xs[4] = (X[20] ^= X[4]);
+        xs[5] = (X[21] ^= X[5]);
+        xs[6] = (X[22] ^= X[6]);
+        xs[7] = (X[23] ^= X[7]);
+        xs[8] = (X[24] ^= X[8]);
+        xs[9] = (X[25] ^= X[9]);
+        xs[10] = (X[26] ^= X[10]);
+        xs[11] = (X[27] ^= X[11]);
+        xs[12] = (X[28] ^= X[12]);
+        xs[13] = (X[29] ^= X[13]);
+        xs[14] = (X[30] ^= X[14]);
+        xs[15] = (X[31] ^= X[15]);
+        for (l = 0; l < 4; l++) {// 8/2
+            xs[4] ^= rotateLeft(xs[0]+xs[12], 7);  xs[8] ^= rotateLeft(xs[4]+xs[0], 9);
+            xs[12] ^= rotateLeft(xs[8]+xs[4],13);  xs[0] ^= rotateLeft(xs[12]+xs[8],18);
+            xs[9] ^= rotateLeft(xs[5]+xs[1], 7);  xs[13] ^= rotateLeft(xs[9]+xs[5], 9);
+            xs[1] ^= rotateLeft(xs[13]+xs[9],13);  xs[5] ^= rotateLeft(xs[1]+xs[13],18);
+            xs[14] ^= rotateLeft(xs[10]+xs[6], 7);  xs[2] ^= rotateLeft(xs[14]+xs[10], 9);
+            xs[6] ^= rotateLeft(xs[2]+xs[14],13);  xs[10] ^= rotateLeft(xs[6]+xs[2],18);
+            xs[3] ^= rotateLeft(xs[15]+xs[11], 7);  xs[7] ^= rotateLeft(xs[3]+xs[15], 9);
+            xs[11] ^= rotateLeft(xs[7]+xs[3],13);  xs[15] ^= rotateLeft(xs[11]+xs[7],18);
+            xs[1] ^= rotateLeft(xs[0]+xs[3], 7);  xs[2] ^= rotateLeft(xs[1]+xs[0], 9);
+            xs[3] ^= rotateLeft(xs[2]+xs[1],13);  xs[0] ^= rotateLeft(xs[3]+xs[2],18);
+            xs[6] ^= rotateLeft(xs[5]+xs[4], 7);  xs[7] ^= rotateLeft(xs[6]+xs[5], 9);
+            xs[4] ^= rotateLeft(xs[7]+xs[6],13);  xs[5] ^= rotateLeft(xs[4]+xs[7],18);
+            xs[11] ^= rotateLeft(xs[10]+xs[9], 7);  xs[8] ^= rotateLeft(xs[11]+xs[10], 9);
+            xs[9] ^= rotateLeft(xs[8]+xs[11],13);  xs[10] ^= rotateLeft(xs[9]+xs[8],18);
+            xs[12] ^= rotateLeft(xs[15]+xs[14], 7);  xs[13] ^= rotateLeft(xs[12]+xs[15], 9);
+            xs[14] ^= rotateLeft(xs[13]+xs[12],13);  xs[15] ^= rotateLeft(xs[14]+xs[13],18);
         }
+        X[16] += xs[0];
+        X[17] += xs[1];
+        X[18] += xs[2];
+        X[19] += xs[3];
+        X[20] += xs[4];
+        X[21] += xs[5];
+        X[22] += xs[6];
+        X[23] += xs[7];
+        X[24] += xs[8];
+        X[25] += xs[9];
+        X[26] += xs[10];
+        X[27] += xs[11];
+        X[28] += xs[12];
+        X[29] += xs[13];
+        X[30] += xs[14];
+        X[31] += xs[15];
     }
 }
