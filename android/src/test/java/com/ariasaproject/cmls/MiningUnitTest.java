6aeb16ee359f994e0cbe5e7bb1697dc99aa9eee0
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-07-06 10:48:53 +0800

    unit test 24

--- android/src/test/java/com/ariasaproject/cmls/MiningUnitTest.java
+++ android/src/test/java/com/ariasaproject/cmls/MiningUnitTest.java
@@ -17,10 +17,9 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.List;
 import java.util.ArrayList;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.*;
- 
 public class MiningUnitTest {
     @Test
     public void HasherTest() throws Exception {
