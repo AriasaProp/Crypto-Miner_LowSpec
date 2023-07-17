5407003a464b3d8222bfe523a8715d1a8d949ceb
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-07-17 12:07:34 +0800

    fix seekbar 2

--- android/src/main/java/com/ariasaproject/cmls/MainActivity.java
+++ android/src/main/java/com/ariasaproject/cmls/MainActivity.java
@@ -129,7 +129,7 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
                 new SeekBar.OnSeekBarChangeListener() {
                     @Override
                     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        cuv.setText(String.format("%03d \%", progress));
+                        cuv.setText(String.format("%03d", progress));
                     }
                     @Override
                     public void onStartTrackingTouch(SeekBar seekBar) {}
