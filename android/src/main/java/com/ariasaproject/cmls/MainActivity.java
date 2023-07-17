c597a9e3b9538db7cb599370de6fef3b99c9e9fc
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-07-17 11:58:09 +0800

    fix seekbar 2

--- android/src/main/java/com/ariasaproject/cmls/MainActivity.java
+++ android/src/main/java/com/ariasaproject/cmls/MainActivity.java
@@ -284,12 +284,12 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
                                 mService.minerStatus[STATUS_TYPE_SPEED] = null;
                             }
                             if (mService.minerStatus[STATUS_TYPE_ACCEPTED] != null) {
-                                AccC = mService.minerStatus[STATUS_TYPE_ACCEPTED]
+                                AccC = mService.minerStatus[STATUS_TYPE_ACCEPTED];
                                 sH.sendEmptyMessage(MSG_UPDATE_ACCEPTED);
                                 mService.minerStatus[STATUS_TYPE_ACCEPTED] = null;
                             }
                             if (mService.minerStatus[STATUS_TYPE_REJECTED] != null) {
-                                rejectC = mService.minerStatus[STATUS_TYPE_REJECTED]
+                                rejectC = mService.minerStatus[STATUS_TYPE_REJECTED];
                                 sH.sendEmptyMessage(MSG_UPDATE_REJECTED);
                                 mService.minerStatus[STATUS_TYPE_REJECTED] = null;
                             }
