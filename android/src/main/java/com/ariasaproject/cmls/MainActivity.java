623c3c56e41297781d71ac8321a640328f9e3012
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-08-09 17:10:12 +0800

    fixup jni

--- android/src/main/java/com/ariasaproject/cmls/MainActivity.java
+++ android/src/main/java/com/ariasaproject/cmls/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        callOfCall();
+        System.out.println(callOfCall());
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
